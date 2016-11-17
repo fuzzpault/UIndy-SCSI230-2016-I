487095f03b21a5eafef65a90b4079e28afb7cf20
Paul Talaga <talagap@uindy.edu>
2016-11-17 16:27:09 +0000

    Commented

--- Week12-Thread2/thread-bad1.cpp
+++ Week12-Thread2/thread-bad1.cpp
@@ -1,9 +1,9 @@
 /*                                                                                                                     
   Name: Paul Talaga
   Date: Nov 1, 2016
-  Desc: Program to demonstrate the use of pthreads
+  Desc: Program to demonstrate why mutexes/locks are needed.
 
-  To compile this, do: g++ -lpthread thread1.cpp
+  To compile this, do: g++ -lpthread thread-bad1.cpp
 */
 
 #include <iostream>
@@ -13,27 +13,29 @@
 
 using namespace std;
 
+// Put parameters you want to send to the threads in this struct.
 struct thread_data_t{
   int thread_id;
-  int value;
 };
 
-int temp;
+int temp;  // Single global variable all threads will update.
 pthread_mutex_t lock;
 
 void* doStuff(void* arg){
   thread_data_t* input;
   input = (thread_data_t*)arg;
+  int a = 0;
   
   int thread_num = input->thread_id;
-  int v = input->value;
-  int a;
+  // Disable all mutexes/locking and see what occurs.
   //pthread_mutex_lock(&lock);
-
-  //cout << thread_num << " value " << v << " temp " << temp << endl;
+  
+  // This should increment temp by 100
   for(int i = 0; i < 100; i++){
     //pthread_mutex_lock(&lock);
     int v = temp;
+    // Do something that will take some time in hopes this thread
+    // will be swaped out for another.
     a = rand();
     temp = v + 1;
     //pthread_mutex_unlock(&lock);
@@ -56,14 +58,16 @@ int main(){
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
   pthread_mutex_init(&lock, NULL);
+
+  // Create 50 threads, all incrementing temp by 100 so temp should equal
+  // 5000 at the end.
   
   for(int i = 0; i < NUM_THREADS; i++){
     passed[i].thread_id = i;
-    passed[i].value = 55;
     pthread_create(&threads[i], &attr, doStuff, (void*)&passed[i]);
   }
 
-
+  // Wait for all threads to finish.
   for(int i = 0; i < NUM_THREADS; i++){
     pthread_join(threads[i], NULL);
     cout << "Thread " << i << " done." << endl;
@@ -71,6 +75,6 @@ int main(){
   pthread_attr_destroy(&attr);
   pthread_mutex_destroy(&lock);
   
-  cout << "Temp: " << temp << endl;
+  cout << "Temp should be 5000 but is: " << temp << endl;
   
 }
